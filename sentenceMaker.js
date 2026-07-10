e82da1936b114aec38ce8b4bb496200f02090f43
syed1005 <syedamad77@gmail.com>
2026-07-10 19:11:03 +0530

    new file added

---
+++ sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "Boy";
+let noun = "Tim";
+let verb = "wet";
+let place = "Volcano";
+let adjective2 = "Salamandar";
+let noun2 = "Piccu";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " +  place +  " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "Man";
+noun = "Cass";
+verb = "fill with fluid";
+place = "Hawaii";
+adjective2 = "Dragon";
+noun2 = "Drogon";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " +  place +  " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log(`Second story: ${secondStory}`);
